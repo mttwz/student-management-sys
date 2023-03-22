3268184a3a1786dcffb347e6cac683190498977d
mttwz <mateevoros@gmail.com>
2023-03-23 00:38:18 +0100

    testfromidea

--- studentmanagementsystem/src/main/java/com/radnoti/studentmanagementsystem/service/UserService.java
+++ studentmanagementsystem/src/main/java/com/radnoti/studentmanagementsystem/service/UserService.java
@@ -123,6 +123,8 @@ public class UserService {
 
         ZonedDateTime currDate = java.time.ZonedDateTime.now();
 
+
+
         User user = userMapper.fromDtoToEntity(userDto);
         user.setPassword(hashUtil.getSHA256Hash(userDto.getPassword()));
         user.setActivationCode(hashUtil.generateRandomString(ACTIVATION_CODE_LENGTH));
