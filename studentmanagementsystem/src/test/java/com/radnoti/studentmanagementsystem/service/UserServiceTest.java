36dc9edebf0c717c76069838a600bda8a0ba78d5
lillarado <105851345+lillarado@users.noreply.github.com>
2023-05-13 12:58:17 +0200

    Update UserServiceTest.java

--- studentmanagementsystem/src/test/java/com/radnoti/studentmanagementsystem/service/UserServiceTest.java
+++ studentmanagementsystem/src/test/java/com/radnoti/studentmanagementsystem/service/UserServiceTest.java
@@ -1,20 +1,32 @@
 package com.radnoti.studentmanagementsystem.service;
 
+import com.radnoti.studentmanagementsystem.enums.SearchFilterEnum;
 import com.radnoti.studentmanagementsystem.exception.form.FormValueInvalidException;
 import com.radnoti.studentmanagementsystem.exception.user.*;
 import com.radnoti.studentmanagementsystem.mapper.UserMapper;
+
+import com.radnoti.studentmanagementsystem.model.dto.PagingDto;
 import com.radnoti.studentmanagementsystem.model.dto.UserDto;
+import com.radnoti.studentmanagementsystem.model.dto.UserInfoDto;
 import com.radnoti.studentmanagementsystem.model.entity.*;
 import com.radnoti.studentmanagementsystem.repository.UserRepository;
 import com.radnoti.studentmanagementsystem.enums.RoleEnum;
 import com.radnoti.studentmanagementsystem.repository.WorkgroupRepository;
 import com.radnoti.studentmanagementsystem.security.HashUtil;
+import com.radnoti.studentmanagementsystem.util.IdValidatorUtil;
+import liquibase.pro.packaged.M;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.ActiveProfiles;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -24,10 +36,13 @@ import java.security.NoSuchAlgorithmException;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.*;
+
 
 
 //@SpringBootTest
@@ -39,6 +54,13 @@ public final class UserServiceTest {
 
     @InjectMocks
     UserService userService;
+
+    @Mock
+    UserDto userDto;
+
+    @Mock
+    UserInfoDto userInfoDtoDto;
+
     @Mock
     UserRepository userRepository;
 
@@ -56,6 +78,9 @@ public final class UserServiceTest {
 
     @Mock
     HashUtil hashUtil;
+
+    @Mock
+    IdValidatorUtil idValidatorUtil;
 
 
     @Test
@@ -220,84 +245,191 @@ public final class UserServiceTest {
 
 
 
-
-
-
     @Test
-    public void deleteUserTest_valid(){
-        //arrange
+    public void testSetUserIsActivated_ValidActivationCodeAndPassword_ActivatesUser() throws NoSuchAlgorithmException {
+        // Arrange
         UserDto userDto = new UserDto();
-        userDto.setId(1);
-        userDto.setIsDeleted(false);
-
-        User mockUser = mock(User.class);
-
-        mockUser.setId(1);
-
-        when(userRepository.findById(any()))
-                .thenReturn(Optional.of(mockUser));
-        //act
-        userService.deleteUser(userDto.getId().toString());
-
-        //assert
-        verify(mockUser,times(1)).setIsDeleted(true);
-    }
-
-    @Test
-    public void deleteUserTest_user_not_exist(){
-        UserDto userDto = new UserDto();
-        userDto.setId(1);
-        userDto.setIsDeleted(false);
-
-
-        when(userRepository.findById(any()))
-                .thenReturn(Optional.empty());
-
-        assertThrows(UserNotExistException.class, ()-> userService.deleteUser(userDto.getId().toString()));
-
-    }
-
-    @Test
-    public void deleteUserTest_user_already_deleted(){
-        UserDto userDto = new UserDto();
-        userDto.setId(1);
-        userDto.setIsDeleted(true);
+        userDto.setActivationCode("123456");
+        userDto.setPassword("password");
 
         User user = new User();
+        user.setActivationCode("123456");
+        user.setPassword(hashUtil.getSHA256Hash("password"));
+
+        when(userRepository.findByActivationCode("123456")).thenReturn(Optional.of(user));
+
+        // Act
+        userService.setUserIsActivated(userDto);
+
+        // Assert
+        verify(userRepository).findByActivationCode("123456");
+        assertTrue(user.getIsActivated());
+        assertNotNull(user.getActivatedAt());
+    }
+
+    @Test
+    public void testSetUserIsActivated_InvalidActivationCode_ThrowsUserNotActivatedException() {
+        // Arrange
+        UserDto userDto = new UserDto();
+        userDto.setActivationCode("invalid");
+        userDto.setPassword("password");
+
+        when(userRepository.findByActivationCode("invalid")).thenReturn(Optional.empty());
+
+        // Act and Assert
+        assertThrows(UserNotActivatedException.class, () -> userService.setUserIsActivated(userDto));
+        verify(userRepository).findByActivationCode("invalid");
+    }
+
+    @Test
+    public void testSetUserIsActivated_IncorrectPassword_ThrowsUserNotActivatedException() throws NoSuchAlgorithmException {
+        // Arrange
+        UserDto userDto = new UserDto();
+        userDto.setActivationCode("123456");
+        userDto.setPassword("incorrect");
+
+        User user = new User();
+        user.setActivationCode("123456");
+        user.setPassword(hashUtil.getSHA256Hash("password"));
+
+        when(userRepository.findByActivationCode("123456")).thenReturn(Optional.empty());
+
+        // Act and Assert
+        assertThrows(UserNotActivatedException.class, () -> userService.setUserIsActivated(userDto));
+    }
+
+    @Test
+    public void testSetUserIsActivated_DeletedUser_ThrowsUserAlreadyDeletedException() throws NoSuchAlgorithmException {
+        // Arrange
+        UserDto userDto = new UserDto();
+        userDto.setActivationCode("123456");
+        userDto.setPassword("password");
+
+        User user = new User();
+        user.setActivationCode("123456");
+        user.setPassword(hashUtil.getSHA256Hash("password"));
         user.setIsDeleted(true);
 
-        when(userRepository.findById(any()))
-                .thenReturn(Optional.of(user));
+        when(userRepository.findByActivationCode("123456")).thenReturn(Optional.of(user));
 
-        assertThrows(UserAlreadyDeletedException.class, ()-> userService.deleteUser(userDto.getId().toString()));
-    }
-
-
-    @Test
-    public void getAllUserTest_valid(){
-
-    }
-
-
-    @Test
-    public void getUserInfoTest_valid(){
+        // Act and Assert
+        assertThrows(UserAlreadyDeletedException.class, () -> userService.setUserIsActivated(userDto));
+        verify(userRepository).findByActivationCode("123456");
 
     }
 
     @Test
-    public void editUserInfoTest_valid(){
+    public void testSetUserIsActivated_AlreadyActivatedUser_ThrowsUserAlreadyActivatedException() throws NoSuchAlgorithmException {
+        // Arrange
+        UserDto userDto = new UserDto();
+        userDto.setActivationCode("123456");
+        userDto.setPassword("password");
 
+        User user = new User();
+        user.setActivationCode("123456");
+        user.setPassword(hashUtil.getSHA256Hash("password"));
+        user.setIsActivated(true);
+
+        when(userRepository.findByActivationCode("123456")).thenReturn(Optional.of(user));
+
+        // Act and Assert
+        assertThrows(UserAlreadyActivatedException.class, () -> userService.setUserIsActivated(userDto));
+        verify(userRepository).findByActivationCode("123456");
+    }
+
+
+
+    @Test
+    public void testDeleteUser_ValidUserId_DeletesUser() {
+        // Arrange
+        String userIdString = "1";
+        Integer userId = idValidatorUtil.idValidator(userIdString);
+        User user = new User();
+        user.setId(userId);
+        user.setIsDeleted(false);
+
+
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        // Act
+        userService.deleteUser(userIdString);
+
+        // Assert
+        verify(userRepository).findById(userId);
+        assertTrue(user.getIsDeleted());
+        assertNotNull(user.getDeletedAt());
     }
 
     @Test
-    public void searchSuperadminTest_valid(){
+    public void testDeleteUser_UserAlreadyDeleted_ThrowsUserAlreadyDeletedException() {
+        // Arrange
+        String userIdString = "1";
+        Integer userId = idValidatorUtil.idValidator(userIdString);
+        User user = new User();
+        user.setId(userId);
+        user.setIsDeleted(true);
 
+        // Configure the findById method to return the user
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        // Act and Assert
+        assertThrows(UserAlreadyDeletedException.class, () -> userService.deleteUser(userIdString));
+    }
+
+
+    @Test
+    public void testDeleteUser_UserNotExist_ThrowsUserNotExistException() {
+        // Arrange
+        String userIdString = "1";
+        Integer userId = idValidatorUtil.idValidator(userIdString);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.empty());
+
+        // Act and Assert
+        assertThrows(UserNotExistException.class, () -> userService.deleteUser(userIdString));
+
+    }
+
+
+    @Test
+    public void testRestoreDeletedUser_ValidUserId_RestoresUser() {
+        // Arrange
+        String userIdString = "1";
+        Integer userId = idValidatorUtil.idValidator(userIdString);
+        User user = new User();
+        user.setId(userId);
+        user.setIsDeleted(true);
+
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        // Act
+        userService.restoreDeletedUser(userIdString);
+
+        // Assert
+        verify(userRepository).findById(userId);
+        assertFalse(user.getIsDeleted());
+        assertNull(user.getDeletedAt());
     }
 
     @Test
-    public void getUserFromWorkgroupTest_valid(){
+    public void testRestoreDeletedUser_UserNotDeleted_ThrowsUserNotDeletedException() {
+        // Arrange
+        String userIdString = "1";
+        Integer userId = idValidatorUtil.idValidator(userIdString);
+        User user = new User();
+        user.setId(userId);
+        user.setIsDeleted(false);
 
+        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+
+        // Act and Assert
+        assertThrows(UserNotDeletedException.class, () -> userService.restoreDeletedUser(userIdString));
     }
+
+
+
+
 
 
 
